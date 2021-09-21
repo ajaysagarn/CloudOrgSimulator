bd6d4f494d878ff707d52386dcf35c140b76d7b3
ajaysagarn <ajaysagarn@gmail.com>
2021-09-21 01:27:32 -0500

    add additional datacenter configs

--- src/main/scala/CloudSimWrappers/ConfiguredDataCenter.scala
+++ src/main/scala/CloudSimWrappers/ConfiguredDataCenter.scala
@@ -5,7 +5,7 @@ import Simulations.BasicCloudSimPlusExample.config
 import Simulations.RandomVmAllocationSim.config
 import com.typesafe.config.Config
 import org.cloudbus.cloudsim.core.Simulation
-import org.cloudbus.cloudsim.datacenters.DatacenterSimple
+import org.cloudbus.cloudsim.datacenters.{Datacenter, DatacenterSimple}
 import org.cloudbus.cloudsim.datacenters.network.NetworkDatacenter
 import org.cloudbus.cloudsim.hosts.{Host, HostSimple}
 import org.cloudbus.cloudsim.resources.PeSimple
@@ -13,10 +13,12 @@ import org.cloudbus.cloudsim.resources.PeSimple
 import collection.JavaConverters.*
 
 class ConfiguredDataCenter(simulation: Simulation, config: Config) {
-  
-  def getConfiguredDataCenter(): DatacenterSimple = {
+
+  def getConfiguredDataCenter(): Datacenter = {
     val hosts: List[Host] = getHostList()
-    new DatacenterSimple(simulation,hosts.asJava)
+    val dc = new DatacenterSimple(simulation,hosts.asJava)
+    dc.setSchedulingInterval(config.getDouble("schedulingInterval"))
+    dc.setVmAllocationPolicy(CloudSimUtils.getDcVmAllocationPolicy(config.getString("VMAllocationPolicy")))
   }
 
   def getHostList(): List[Host] = {
@@ -24,11 +26,12 @@ class ConfiguredDataCenter(simulation: Simulation, config: Config) {
     List.fill(hostCount)(new HostSimple(config.getLong("host.RAMInMBs"),
       config.getLong("host.StorageInMBs"),
       config.getLong("host.BandwidthInMBps"),
-      getPes().asJava).setVmScheduler(CloudSimUtils.getVmScheduler(config.getString("host.VMschedulerType"))))
+      getPes().asJava)
+      .setVmScheduler(CloudSimUtils.getVmScheduler(config.getString("host.VMschedulerType"))))
   }
-  
+
   def getPes(): List[PeSimple] = {
     List.fill(config.getInt("host.Pes"))(new PeSimple(config.getDouble("host.mipsCapacity")))
   }
-  
+
 }
