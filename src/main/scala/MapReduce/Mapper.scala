b8d125549ec86ae4e97354e16be2d7c77564c1a4
ajaysagarn <ajaysagarn@gmail.com>
2021-09-21 22:28:23 -0500

    Add cloudlet Type

--- src/main/scala/MapReduce/Mapper.scala
+++ src/main/scala/MapReduce/Mapper.scala
@@ -1,10 +1,12 @@
 package MapReduce
 
 import org.cloudbus.cloudsim.cloudlets.CloudletSimple
+import org.cloudbus.cloudsim.cloudlets.network.NetworkCloudlet
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel
 
 
 class Mapper (length: Long, pesNumber: Int) extends CloudletSimple(length,pesNumber) {
-
+  private val TYPE = "MAPPER"
+  def getType = TYPE
 }
 
--- src/main/scala/MapReduce/Reducer.scala
+++ src/main/scala/MapReduce/Reducer.scala
@@ -1,8 +1,10 @@
 package MapReduce
 
 import org.cloudbus.cloudsim.cloudlets.CloudletSimple
+import org.cloudbus.cloudsim.cloudlets.network.NetworkCloudlet
 import org.cloudbus.cloudsim.utilizationmodels.UtilizationModel
 
 class Reducer(length: Long, pesNumber: Int) extends CloudletSimple(length,pesNumber) {
-
+  private val TYPE = "REDUCER"
+  def getType = TYPE
 }
