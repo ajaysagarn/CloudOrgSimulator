72c3c03fddb14aeb732c7b847226022639bf2614
ajaysagarn <ajaysagarn@gmail.com>
2021-09-21 22:29:27 -0500

    Add pricing characteristics to datacenter

--- src/main/scala/CloudSimWrappers/ConfiguredDataCenter.scala
+++ src/main/scala/CloudSimWrappers/ConfiguredDataCenter.scala
@@ -8,18 +8,34 @@ import org.cloudbus.cloudsim.core.Simulation
 import org.cloudbus.cloudsim.datacenters.{Datacenter, DatacenterSimple}
 import org.cloudbus.cloudsim.datacenters.network.NetworkDatacenter
 import org.cloudbus.cloudsim.hosts.{Host, HostSimple}
+import org.cloudbus.cloudsim.provisioners.{PeProvisionerSimple, ResourceProvisionerSimple}
 import org.cloudbus.cloudsim.resources.PeSimple
 
 import collection.JavaConverters.*
 
 class ConfiguredDataCenter(simulation: Simulation, config: Config) {
 
-  def getConfiguredDataCenter(): Datacenter = {
+  val dataCenter = createDatacenter()
+
+  def getConfiguredDataCenter(): Datacenter = dataCenter
+
+  def createDatacenter(): Datacenter = {
     val hosts: List[Host] = getHostList()
-    val dc = new DatacenterSimple(simulation,hosts.asJava)
+    val dc = new NetworkDatacenter(simulation,hosts.asJava,CloudSimUtils.getDcVmAllocationPolicy(config.getString("VMAllocationPolicy")))
     dc.setSchedulingInterval(config.getDouble("schedulingInterval"))
-    dc.setVmAllocationPolicy(CloudSimUtils.getDcVmAllocationPolicy(config.getString("VMAllocationPolicy")))
+
+    dc.getCharacteristics()
+      .setCostPerStorage(config.getDouble("costPerStorage"))
+      .setCostPerSecond(config.getDouble("costPerCPUSecond"))
+      .setCostPerMem(config.getDouble("costPerMemory"))
+      .setCostPerBw(config.getDouble("costPerBW"))
+/*      .setOs(config.getString("schedulingInterval"))
+      .setVmm(config.getString("schedulingInterval"))*/
+    dc
+
   }
+
+
 
   def getHostList(): List[Host] = {
     val hostCount = if (config.getString("host.CreationType") == "static") config.getInt("host.Count") else 0
@@ -27,11 +43,13 @@ class ConfiguredDataCenter(simulation: Simulation, config: Config) {
       config.getLong("host.StorageInMBs"),
       config.getLong("host.BandwidthInMBps"),
       getPes().asJava)
-      .setVmScheduler(CloudSimUtils.getVmScheduler(config.getString("host.VMschedulerType"))))
+      .setVmScheduler(CloudSimUtils.getVmScheduler(config.getString("host.VMschedulerType")))
+      .setRamProvisioner(ResourceProvisionerSimple())
+      .setBwProvisioner(ResourceProvisionerSimple()))
   }
 
   def getPes(): List[PeSimple] = {
-    List.fill(config.getInt("host.Pes"))(new PeSimple(config.getDouble("host.mipsCapacity")))
+    List.fill(config.getInt("host.Pes"))(new PeSimple(config.getDouble("host.PemipsCapacity"),PeProvisionerSimple()))
   }
 
 }
