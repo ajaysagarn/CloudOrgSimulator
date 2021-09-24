7e43c22c2451de42c16fc23fef0470dc50666129
ajaysagarn <ajaysagarn@gmail.com>
2021-09-23 23:48:09 -0500

    Modify config ref to fetch multiple files

--- src/main/scala/HelperUtils/ObtainConfigReference.scala
+++ src/main/scala/HelperUtils/ObtainConfigReference.scala
@@ -4,13 +4,25 @@ import com.typesafe.config.{Config, ConfigFactory}
 
 import scala.util.{Failure, Success, Try}
 
-class ObtainConfigReference
+class ObtainConfigReference(fileName:String){
+  def getFileName = fileName
+}
 object ObtainConfigReference:
-  private val config = ConfigFactory.load()
   private val logger = CreateLogger(classOf[ObtainConfigReference])
-  private def ValidateConfig(confEntry: String):Boolean = Try(config.getConfig(confEntry)) match {
+  
+  private def ValidateConfig(config:Config,confEntry: String):Boolean = Try(config.getConfig(confEntry)) match {
     case Failure(exception) => logger.error(s"Failed to retrieve config entry $confEntry for reason $exception"); false
     case Success(_) => true
   }
 
-  def apply(confEntry:String): Option[Config] = if ValidateConfig(confEntry) then Some(config.getConfig(confEntry)) else None
+  def getConfig(file:String,configPath:String): Option[Config] = {  
+    val config:Config = Try(ConfigFactory.load(file)) match {
+      case Failure(exception) => logger.error(s"Failed to retrieve config file $file for reason $exception"); ConfigFactory.load()
+      case Success(_) => ConfigFactory.load(file)
+    }
+
+    if ValidateConfig(config,configPath) then Some(config.getConfig(configPath)) else None
+    
+  }
+
+  def apply(fileName:String,confPath:String): Option[Config] = getConfig(fileName,confPath)
