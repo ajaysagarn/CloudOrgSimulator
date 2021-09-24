df50b049b4247fa139e8b8f17549374fd1062e28
ajaysagarn <ajaysagarn@gmail.com>
2021-09-24 02:26:02 -0500

    remove comments

--- src/main/scala/Simulations/HorizontalVsVerticalScaling.scala
+++ src/main/scala/Simulations/HorizontalVsVerticalScaling.scala
@@ -17,55 +17,6 @@ object HorizontalVsVerticalScaling:
   @main
   def startHorzontalVsVertical(): Unit ={
 
-
-/*    val config: Config = CloudSimUtils.getconfigValue("VerticalVHorizontalScale","HorizontalScaling")
-
-    val logger = CreateLogger(classOf[StartSimulation])
-    val simulation = new CloudSim()
-    val configDatacenter = new ConfiguredDataCenter(simulation,config)
-    val datacenter = new ConfiguredDataCenter(simulation,config).getConfiguredDataCenter()
-    //configDatacenter.createNetwork(datacenter)
-    logger.info("Datacenter created!")
-
-    val broker = new ConfiguredBroker(simulation,config).getConfiguredBroker()
-
-    logger.info("Broker created!")
-
-    val vms = new ConfiguredVm(simulation,broker,config)
-    vms.submitinitialVms()
-
-    logger.info("Vms created as per configuration and submitted to broker")
-    // Finally start a map reduce job which dynamically submits the requests to the broker
-
-    val mapreduceConfig: Config = CloudSimUtils.getconfigValue("mapreduce","MapReduceJob")
-
-    val mapReduceJob = new MapReduceJob(simulation,broker,mapreduceConfig)
-
-    val mapperCloudletList = mapReduceJob.getMappers()
-
-    broker.submitCloudletList(mapReduceJob.getMappers().asJava)
-
-    logger.info("Cloudlets submitted to broker")
-    simulation.start()
-
-    new CloudletsTableBuilder(broker.getCloudletFinishedList()).build();
-
-    val vmLogs = vms.getVmTimeLogs
-    vms.getVmTimeLogs.foreach(log => {
-      logger.info(log)
-    })
-
-    val vmsCreated = broker.getVmCreatedList[NetworkVm]
-    val total:CostTotals  = CloudSimUtils.VmCostMetrics(vmsCreated.asScala.toList, true)
-
-    //print(total)
-
-    val saasmapperCosts = CloudSimUtils.cost1(datacenter,mapReduceJob.getMappers())
-    val saasreducerCosts = CloudSimUtils.cost1(datacenter,mapReduceJob.getReducers())
-    println(saasmapperCosts + saasreducerCosts)*/
-
-
-
     val configHorizSim: Config = CloudSimUtils.getconfigValue("VerticalVHorizontalScale","HorizontalScaling")
     val configVertSim: Config = CloudSimUtils.getconfigValue("VerticalVHorizontalScale","VerticalScaling")
 
@@ -119,5 +70,5 @@ object HorizontalVsVerticalScaling:
     val vmsCreatedVscale = brokerVScale.getVmCreatedList[NetworkVm]
     val totalVs:CostTotals  = CloudSimUtils.VmCostMetrics(vmsCreatedVscale.asScala.toList, true)
     CloudSimUtils.printCostTotals(totalVs)
-    
+
   }
